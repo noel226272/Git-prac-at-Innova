0f43b322ec2d9bc21c747c33e3e3b337948691ab
noel226272 <awanoel43@gmail.com>
2026-08-13 12:59:52 +0200

    Add function to reverse a string

--- functions.js
+++ functions.js
@@ -7,12 +7,19 @@ function isEven(number) {
 console.log("Is 10 even?", isEven(10));
 
 
+
 // Function 2: Find the largest of three numbers
 function findLargest(a, b, c) {
     return Math.max(a, b, c);
 }
-
 // Test Function 2
 console.log("Largest number:", findLargest(10, 25, 15));
 
 
+// Function 3: Reverse a string
+function reverseString(text) {
+    return text.split("").reverse().join("");
+}
+
+// Test Function 3
+console.log("Reversed string:", reverseString("hello"));
